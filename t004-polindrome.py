3d6e8308beca956cd550c77c97f8f5e0c60741b5
Дмитрий Борисов <z1365@mail.ru>
2019-08-15 11:48:03 +0300

    T004. Решена.

---
+++ t004-polindrome.py
@@ -0,0 +1,44 @@
+import os
+from colorama import init, deinit, Fore, Style   # Back
+import time
+import datetime
+
+init()
+
+# cls + Шапка
+os.system('cls')
+print('{:*^80}'.format('  \x1b[33;1mПроект Эйлера\x1b[0m  '))
+print('{:^80}'.format('\x1b[32mhttp://projecteuler.net/archives\x1b[0m'))
+
+# Task 004
+print('''
+Задача 004:
+===========
+\x1b[30;1mA palindromic number reads the same both ways.
+The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+\x1b[0m
+Полиндромы это числа, которые читаются одинаково как слева направо, так и справа налево.
+Наибольший полиндром, полученный произведением двухчисленных числителей: 9009 = 91 х 99.
+
+\x1b[33mFind the largest palindrome made from the product of two 3-digit numbers.
+\x1b[33;1mНайти наибольший полиндром, полученный путем произведения трёх-цифровых числителей.
+''')
+print(Style.RESET_ALL)
+
+start_time = time.time()
+largest_polindrome = 0
+for first in range(100, 1000):
+    for second in range(10, 1000):
+        testing = first * second
+        s1 = str(testing)
+        s2 = s1[::-1]
+        if s1 == s2:
+            if largest_polindrome < testing:
+                largest_polindrome = testing
+                # print('{} \x1b[32;1mэквивалентно \x1b[0m{}'.format(s1, s2))
+
+print("Ответ: " + Fore.GREEN + Style.BRIGHT + str(largest_polindrome) + Style.RESET_ALL)
+end_time = time.time()
+delta_time = end_time - start_time
+print(Fore.RED + Style.BRIGHT + 'Работало ' + str(datetime.timedelta(seconds=delta_time)) + Style.RESET_ALL)
+deinit()
